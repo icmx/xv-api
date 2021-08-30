dced4fcf5a8945681cb1e389fb8ab4c90ff43db8
icmx <32983315+icmx@users.noreply.github.com>
2021-08-30 10:13:38 +0300

    add shared and xkcd feature modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+import { XkcdComicsModule } from './xkcd-comics/xkcd-comics.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [XkcdComicsModule],
 })
 export class AppModule {}
